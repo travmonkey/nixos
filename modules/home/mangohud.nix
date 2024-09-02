fbfe2d04c947439174c9511d8157c81066a4de4c
travmonkey <t.coding@batk.me>
2024-09-01 18:06:50 -0600

    Configure mangohud

--- modules/home/mangohud.nix
+++ modules/home/mangohud.nix
@@ -9,7 +9,23 @@
     enable = true;
 
     settings = {
-      fps_limit = 15;
+      # FPS related
+      fps_limit = 0;
+      frametime = true;
+      frame_timing = 0;
+
+      # CPU related
+      cpu_temp = true;
+      cpu_load_change = true;
+      cpu_load_color = "b8bb26,fabd2f,fb4934";
+
+      # GPU related
+      gpu_temp = true;
+      gpu_load_change = true;
+      gpu_load_color = "b8bb26,fabd2f,fb4934";
+
+      # RAM related
+      ram = true;
     };
   };
 }
