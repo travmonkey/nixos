5f8f84a2e67e650a798a2e858e368e69ba87ddc8
travmonkey <t.coding@batk.me>
2024-09-07 00:59:33 -0600

    Configure dunst

--- modules/home/dunst.nix
+++ modules/home/dunst.nix
@@ -5,11 +5,29 @@
     dunst
   ];
 
-  programs.dunst = {
+  services.dunst = {
     enable = true;
     
     settings = {
+      global = {
+        monitor = 1;
 
+        width = 300;
+        height = 300;
+
+        origin = "top-right";
+
+        frame_color = "#d65d0e";
+
+        offset = "16x30";
+
+        corner_radius = 16;
+
+        frame_width = 1;
+
+        background = "#282828";
+        foreground = "#ebdbb2";
+      };
     };
   };
 }
