2db4bff37b4053bfd83c4164badc1aa759756c3d
travmonkey <t.coding@batk.me>
2024-09-27 09:20:26 -0600

    Remove dunst

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -6,11 +6,10 @@
     ++[(import ./zsh.nix)]
     ++[(import ./wlogout/wlogout.nix)]
     ++[(import ./spicetify.nix)]
-    ++[(import ./dunst.nix)]
+    # ++[(import ./dunst.nix)]
     ++[(import ./waybar.nix)]
     ++[(import ./mangohud.nix)]
     ++[(import ./btop.nix)]
-    # ++[(import ./sddm-theme.nix)]
     ++[(import ./ags/ags.nix)]
     ++[(import ./kitty.nix)];
 }
