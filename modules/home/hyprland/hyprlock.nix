449f172612818f8bb1366833ccd10658448a03aa
travmonkey <t.coding@batk.me>
2024-09-28 02:28:28 -0600

    Fix hyprlock and configure

--- modules/home/hyprland/hyprlock.nix
+++ modules/home/hyprland/hyprlock.nix
@@ -7,7 +7,7 @@
     settings = {
       background = {
         monitor = "";
-        path = "/home/travis/Pictures/wallpapers/Anime-Lanscape.png";
+        path = "/home/travis/Pictures/wallpapers/ciri_geralt.jpg";
         blur_size = "2";
         blur_passes = "2";
         noise = "0.0117";
@@ -24,9 +24,9 @@
         dots_size = "0.26";
         dots_spacing = "0.64";
         dots_center = "true";
-        outer_color = "rgb(245, 189, 230)";
-        inner_color = "rgb(36, 39, 58)";
-        font_color = "rgb(202, 211, 245)";
+        outer_color = "rgb(177, 98, 134)";
+        inner_color = "rgb(40, 40, 40)";
+        font_color = "rgb(177, 98, 134)";
         fade_on_empty = "true";
         placeholder_text = "<i>Password...</i>";
         hide_input = "false";
@@ -36,19 +36,32 @@
         valign = "bottom";
       };
 
-      label = {
+      label = [
+        {
         monitor = "DP-1";
         text = ''cmd[update:1000] echo "<b><big> $(date +"%I:%M:%S $p") </big></b>"'';
-        color = "rgb(245, 189, 230)";
+        color = "rgb(177, 98, 134)";
         font_size = "64";
-        font_family = "Monofur Nerd Font 10";
+        font_family = "Mononoki Nerd Font 10";
         shadow_passes = "3";
         shadow_size = "4";
 
         position = "0, 16";
         halign = "center";
         valign = "center";
-      };
+        }
+        {
+          monitor  = "DP-1";
+          text = "Hey $USER";
+          color = "rgb(177, 98, 134)";
+          font_size = 18;
+          font_family = "Monofur Nerd Font";
+          position = "0, 30";
+          halign = "center";
+          valign = "bottom";
+
+        }
+      ];
     };
   };
 }
