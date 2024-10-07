f7778d82c0eae75b47c15695e74f1a17d1838714
travmonkey <t.coding@batk.me>
2024-10-07 16:39:30 -0600

    Add prettier

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -2,11 +2,12 @@
 
 {
   # Language servers and formatter for neovim
+  # Attempting to keep this low in favor of using shell.nix, but it gets quite annoying
+  # entering a shell.nix for frequent languages
   home.packages = with pkgs; [
     neovim
 
     # Language Servers
-    llvmPackages_12.libcxxClang # c/c++
     lua-language-server # lua
     nodePackages_latest.bash-language-server # bash
     nodePackages_latest.typescript-language-server # ts/js
@@ -18,6 +19,7 @@
     isort
     shfmt
     mdformat
+    nodePackages_latest.prettier
 
     # Linters
     eslint_d
