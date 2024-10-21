cbd4565be38d16280a187155dda75685abb086b4
travmonkey <t.coding@batk.me>
2024-10-21 14:19:22 -0600

    Remove ags and split-monitor-workspaces input

--- flake.nix
+++ flake.nix
@@ -9,17 +9,10 @@
     };
     # Hyprland related stuff
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-    split-monitor-workspaces = {
-      url = "github:Duckonaut/split-monitor-workspaces";
-      inputs.hyprland.follows = "hyprland";
-    };
     hyprland-plugins = {
       url = "github:hyprwm/hyprland-plugins";
       inputs.hyprland.follows = "hyprland";
     };
-
-    # add ags
-    ags.url = "github:Aylur/ags";
 
     # add stylix
     stylix.url = "github:danth/stylix";
