d742e7aee9334e533614851bf607494016458070
travmonkey <t.coding@batk.me>
2024-10-24 00:57:16 -0600

    Fix monitors turning off

--- modules/home/hyprland/hypridle.nix
+++ modules/home/hyprland/hypridle.nix
@@ -12,17 +12,19 @@
         after_sleep_cmd = "hyprctl dispatch dpms on";
       };
 
-      listener = [{
-        timeout = "300"; # 5min
-        on-timeout =
-          "loginctl lock-session"; # lock screen when timeout has passed
-      }
-      # {
-      #   timeout = "15";                                 # 5.5min
-      #   on-timeout = "hyprctl dispatch dpms off";       # screen off when timeout has passed
-      #   on-resume = "hyprctl dispatch dpms on";
-      # }
-        ];
+      listener = [
+        {
+          timeout = "300"; # 5min
+          on-timeout =
+            "loginctl lock-session"; # lock screen when timeout has passed
+        }
+        {
+          timeout = "15"; # 5.5min
+          on-timeout =
+            "hyprctl keyword monitor HDMI-A-1, disable && hyprctl keyword monitor DP-1, disable && hyprctl keyword monitor DP-2, disable &&"; # I LOVE hdmi monitors
+          on-resume = "hyprctl reload";
+        }
+      ];
     };
   };
 }
